a19a8448495d7fb804b7b89ae1de0e9cc18cd120
Gabriel RiCharde <gabe.richarde@my.wheaton.edu>
2016-08-15 15:50:48 +0000

    Created ruby basic best practice file

---
+++ ruby_best_practices.rb
@@ -0,0 +1,37 @@
+# "Naming a file in snake case"
+
+this_is_a_snake_cased_file.rb
+
+# Assiging a variable
+
+forty_two = 42
+
+# Defining a method
+
+def this_is_a_great_method
+  # do stuff
+end
+
+# Constant declaration
+
+FOUR = 'four'
+FIVE = 5
+
+# Multi-line vs single line
+
+[1, 2, 3].each do |i|
+  # do stuff
+end
+
+[1, 2, 3].each { |i| }
+
+# Class naming with CamelCase
+
+class TheFirstClass
+end
+
+class TheSecondClass
+end
+
+
+
